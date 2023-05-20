1c73fa192fefd1512bd937c0998696bdc20cb073
goncas200 <111070886+goncas200@users.noreply.github.com>
2023-05-20 22:45:39 +0100

    Adicionar arquivos de projeto.

---
+++ FileReader.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+using namespace std;
+using namespace filesystem;
+
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
+
