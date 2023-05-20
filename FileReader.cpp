386aa0c0fef6cc19072c037b76910b9cf78967ca
goncas200 <111070886+goncas200@users.noreply.github.com>
2023-05-20 23:32:02 +0100

    Release

--- FileReader.cpp
+++ FileReader.cpp
@@ -4,8 +4,38 @@
 using namespace std;
 using namespace filesystem;
 
-int main() {
-    cout << "Hello World!\n";
+void read_file(string file_name) {
+    ifstream file;
+    file.open(file_name);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            cout << line << "\n";
+        }
+        file.close();
+    }
+    else
+        cout << "Couldn't open the file\n";
+}
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        string filename;
+        cout << "What's the filename?: ";
+        getline(cin, filename);
+        read_file(filename);
+    }
+    else if (argc == 2) {
+        current_path(argv[1]);
+        string filename;
+        cout << "What's the filename?: ";
+        getline(cin, filename);
+        read_file(filename);
+    }
+    else {
+        cout << "Error: There are too much arguments";
+        return 1;
+    }
     return 0;
 }
 
